f6bc38619c22373db8dee7357f66ed08b90f6f53
Darshan Heble <darshanheble@gmail.com>
2026-09-09 19:56:56 +0530

    Refactor button click event for better readability

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const showDetailsButton = document.getElementById("show-details-btn");
 const studentInfo = document.getElementById("student-info");
 
 showDetailsButton.addEventListener("click", () => {
-	const detailsAreVisible = studentInfo.style.display !== "none";
-	studentInfo.style.display = detailsAreVisible ? "none" : "block";
-	showDetailsButton.textContent = detailsAreVisible ? "Show Details" : "Hide Details";
+  const detailsAreVisible = studentInfo.style.display !== "none";
+  studentInfo.style.display = detailsAreVisible ? "none" : "block";
+  showDetailsButton.textContent = detailsAreVisible
+    ? "Show Details"
+    : "Hide Details";
 });
