ba65b8a8873c392dd5264f4b30318ecc4c3ebf37
Arjit-14 <arjitsinghal2004@gmail.com>
2026-09-09 19:40:15 +0530

    add files

---
+++ script.js
@@ -0,0 +1,3 @@
+/* Student 3 will add functionality to this file later in their feature/javascript branch */
+// The goal will be to make the "Show Details" button display the student info div.
+console.log("App loaded. JS functionality to be added.");
